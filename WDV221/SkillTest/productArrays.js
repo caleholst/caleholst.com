dba8b7330b136f6d0deaf52e1f4e781ccf077702
Cale Holst <caleholst@gmail.com>
2024-04-29 15:56:35 -0500

    did skill test 2

---
+++ WDV221/SkillTest/productArrays.js
@@ -0,0 +1,20 @@
+//Product data:
+			
+	const productNames = ['digital camera','headset with microphone','wireless mouse'];
+	const productNumbers = ['dc-25W','h-24 WFSD','wm-js-usb'];
+	const productManPrices = [14.90,32.87,6.54];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
